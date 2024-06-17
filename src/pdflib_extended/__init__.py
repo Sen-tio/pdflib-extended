2176d51f1b6d9aeaa65f33c735d08465fcc6d168
RichardS <richards@eoshost.com>
2024-06-17 11:38:11 -0700

    added TETLib to top-level __init__.py for easy importing

--- src/pdflib_extended/__init__.py
+++ src/pdflib_extended/__init__.py
@@ -1,5 +1,6 @@
 from .pdflib import PDFlib
+from .tetlib import TETLib
 from .extensions.classes import Box, Point
 
 __version__ = "2024.06.12"
-__all__ = ["PDFlib", "Box", "Point"]
+__all__ = ["PDFlib", "TETLib", "Box", "Point"]
